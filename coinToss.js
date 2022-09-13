c88826429c2ad3ecd2a6f3919c64adf536d64040
Vishnu Varthanan T S <112369906+vichu5101@users.noreply.github.com>
2022-09-13 17:53:28 +0530

    Update coinToss.js

--- coinToss.js
+++ coinToss.js
@@ -1,12 +1,13 @@
+console.log("The game will start in next 15th sec...")
 player1 = []
 player2 = []
 playerScoreObj = {
     player1Heads: 0,
     player2Heads: 0
 }
-roundPlayerScoreObj={
-    player1Heads:0,
-    player2Heads:0
+roundPlayerScoreObj = {
+    player1Heads: 0,
+    player2Heads: 0
 }
 a = setInterval(() => {
     coinToss()
@@ -16,25 +17,18 @@ function coinToss() {
     if (round == 5) {
         clearInterval(a)
         console.log("********** From All Rounds ************")
-        if (playerScoreObj.player1Heads > playerScoreObj.player2Heads) {
-            console.log("Player1 Won the game")
-        }
-        else if (playerScoreObj.player1Heads < playerScoreObj.player2Heads) {
-            console.log("Player2 Won the game")
-        }
-        else {
-            console.log("Game Draw")
-        }
+        console.table([playerScoreObj])
+        result(playerScoreObj.player1Heads, playerScoreObj.player2Heads)
     }
     else {
-        console.log("Round :",round)
+        console.log("Round :", round)
         coinTossing()
         round++
     }
 }
 
 function coinTossing() {
-    return new Promise((resolve) => {
+    return new Promise(() => {
         player1CoinToss = []
         player2CoinToss = []
         range = 0
@@ -44,17 +38,9 @@ function coinTossing() {
                 clearInterval(b)
                 findScore(player1CoinToss, player2CoinToss)
                 console.table([roundPlayerScoreObj])
-                if (roundPlayerScoreObj.player1Heads > roundPlayerScoreObj.player2Heads) {
-                    console.log("Player1 Won the game")
-                }
-                else if (roundPlayerScoreObj.player1Heads < roundPlayerScoreObj.player2Heads) {
-                    console.log("Player2 Won the game")
-                }
-                else {
-                    console.log("Game Draw")
-                }
-                roundPlayerScoreObj["player1Heads"]=0
-                roundPlayerScoreObj["player2Heads"]=0
+                result(roundPlayerScoreObj.player1Heads, roundPlayerScoreObj.player2Heads)
+                roundPlayerScoreObj["player1Heads"] = 0
+                roundPlayerScoreObj["player2Heads"] = 0
             }
             else {
                 p1 = face[Math.floor(Math.random() * 2)]
@@ -63,7 +49,7 @@ function coinTossing() {
                 player2CoinToss.push(p2)
                 player1.push(p1)
                 player2.push(p2)
-                console.log(p1, p2)
+                console.log("Player1 : ", p1, "-----", "Player2 :", p2)
                 range++
             }
         }, 1000);
@@ -74,13 +60,26 @@ function findScore(player1List, player2List) {
     for (i of player1List) {
         if (i == "Heads") {
             playerScoreObj.player1Heads += 1
-            roundPlayerScoreObj.player1Heads+=1
+            roundPlayerScoreObj.player1Heads += 1
         }
     }
     for (i of player2List) {
         if (i == "Heads") {
             playerScoreObj.player2Heads += 1
-            roundPlayerScoreObj.player2Heads+=1
+            roundPlayerScoreObj.player2Heads += 1
         }
     }
+}
+
+function result(player1, player2) {
+    if (player1 > player2) {
+        console.log("Player 1 Won the Game")
+    }
+    else if (player1 < player2) {
+        console.log("Player 2 Won the Game")
+    }
+    else {
+        console.log("Match Draw")
+    }
+    console.log("---------------------------------------------------------------------")
 }
